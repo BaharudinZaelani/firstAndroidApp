89e4da8c1a3ddacf9f1be70541daf240e40775ba
Zaelani <bahar32harz@gmail.com>
2023-08-31 15:37:20 +0700

    Update Icon

--- app/src/main/java/com/example/baharapp/HomeActivity.kt
+++ app/src/main/java/com/example/baharapp/HomeActivity.kt
@@ -7,17 +7,24 @@ import androidx.appcompat.app.AppCompatActivity
 
 class HomeActivity: AppCompatActivity() {
     private lateinit var btnMove: Button
+    private lateinit var btnMoveToIntent: Button
     private fun constructor() {
         btnMove = findViewById(R.id.btn_move_activity)
+        btnMoveToIntent = findViewById(R.id.btnMoveToIntent)
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         constructor()
 
+        // Move to Latihan 1
         btnMove.setOnClickListener {
-            val halloActivity = Intent(this@HomeActivity, HalloDunia::class.java)
-            startActivity(halloActivity)
+            startActivity(Intent(this@HomeActivity, HalloDunia::class.java))
+        }
+
+        // Move to Latihan 2
+        btnMoveToIntent.setOnClickListener {
+            startActivity(Intent(this@HomeActivity, IntentActivity::class.java))
         }
     }
 
---
+++ app/src/main/java/com/example/baharapp/IntentActivity.kt
@@ -0,0 +1,12 @@
+package com.example.baharapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+class IntentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intent)
+    }
+
+}
