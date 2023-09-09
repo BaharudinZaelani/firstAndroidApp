dd9fa8d48b1be8e196d8fd7a9eb582c2b658b126
Zaelani <bahar32harz@gmail.com>
2023-09-09 11:59:52 +0700

    Fragment Page...

--- app/src/main/java/com/example/baharapp/HomeActivity.kt
+++ app/src/main/java/com/example/baharapp/HomeActivity.kt
@@ -4,16 +4,19 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 
 class HomeActivity: AppCompatActivity() {
     private lateinit var btnMove: Button
     private lateinit var btnMoveToIntent: Button
     private lateinit var btnRcycleViewer: Button
+    private lateinit var btnFragment: Button
     private fun constructor() {
         btnMove = findViewById(R.id.btn_move_activity)
         btnMoveToIntent = findViewById(R.id.btnMoveToIntent)
         btnRcycleViewer = findViewById(R.id.btnRecyclerView)
+        btnFragment = findViewById(R.id.btnFragmen)
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,7 +36,28 @@ class HomeActivity: AppCompatActivity() {
         // Move to Latihan 3 : Recycle Viewer
         btnRcycleViewer.setOnClickListener {
             startActivity(Intent(this@HomeActivity, RecycleActivity::class.java))
-            Log.i("ZAW", "Test")
+        }
+
+        // Move to Fragment Page
+        val supportFragment = supportFragmentManager
+        val homeFragment = HomeFragment()
+        val fragment = supportFragment.findFragmentByTag(HomeFragment::class.java.simpleName)
+
+        val container: LinearLayout = findViewById(R.id.container_fragment)
+        btnFragment.setOnClickListener {
+            if ( fragment !is HomeFragment ) {
+                if ( container.childCount == 0 ) {
+                    supportFragment
+                        .beginTransaction()
+                        .add(R.id.container_fragment, homeFragment)
+                        .commit()
+                }else {
+                    supportFragment
+                        .beginTransaction()
+                        .remove(homeFragment)
+                        .commit()
+                }
+            }
         }
     }
 
